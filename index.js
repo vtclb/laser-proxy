7937ad8a434bb26821b1fd986980de73702f721a
vtclb <vartaclub@gmail.com>
2025-07-18 22:51:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ export default {
     if (request.method === 'POST') {
       const raw = await request.text();
       const resp = await fetch(
-        'https://script.google.com/macros/s/AKfycbwNfN2Gc5i6bmZrI6m_kbj9iSgGKw4AufWP5MEe1MZ1jP4-Wvtq0RThdt0lc7u-93POrw/exec',
+        'https://script.google.com/macros/s/AKfycbzriJr6tyZse9jivlxjNauaNOjMZTLLYuCC88o46iwr7vL4peFmFMASvB1kzFSCanUy-g/exec',
         { method: 'POST', headers: { 'Content-Type': 'application/x-www-form-urlencoded' }, body: raw }
       );
       const t = await resp.text();
