5067b11082bb0efe403a338ef16ef9979041e182
vtclb <vartaclub@gmail.com>
2026-02-04 03:50:16 +0200

    Update BACKEND URL in index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ export default {
     const url = new URL(request.url);
 
     // ---- CONFIG: жорстко вшитий GAS /exec ----
-    const BACKEND = 'https://script.google.com/macros/s/AKfycbwHfFXlLUodqMVpv6NJnR1yYwYwm7_MG9gwJYZlALcoqp8ZICcQar_aPwDil7g0R7rd5A/exec';
+    const BACKEND = 'https://script.google.com/macros/s/AKfycbwWws5KHALVToh4J3sQtRsQ5eaOftJr_leyell2d8NavGBlFdIgxufN8u6fQAxec73lyw/exec';
 
     // ---- CORS (віддзеркалюємо Origin) ----
     const origin = request.headers.get('Origin') || '*';
