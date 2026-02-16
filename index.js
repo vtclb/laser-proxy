203f34557633095a44b6ed4c9db7df80d5a0a158
vtclb <vartaclub@gmail.com>
2026-02-16 14:57:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ export default {
     const url = new URL(request.url);
 
     // ---- CONFIG: жорстко вшитий GAS /exec ----
-    const BACKEND = 'https://script.google.com/macros/s/AKfycbwn6xtYqiQY0xFUI6flRas7Td0gttWATmNGV076jUh8KIw9-YphrKk37wxgsWuwVMAmzg/exec';
+    const BACKEND = 'https://script.google.com/macros/s/AKfycbz8VE2T92wCFKFU_xbiFdCg7M_-x7MS-Pg_5zMcxDTwKxVIJWpA81KPbHTa7a-G1gwKxw/exec';
 
     // ---- CORS (віддзеркалюємо Origin) ----
     const origin = request.headers.get('Origin') || '*';
